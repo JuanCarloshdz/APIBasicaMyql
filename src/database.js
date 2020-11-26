030a4b7829383cca2a0897b7ad6b9bf3a6a57107
zyttor <zyttor@gmail.com>
2020-11-26 15:12:50 -0600

    Configuracion DB heroku

--- src/database.js
+++ src/database.js
@@ -1,12 +1,16 @@
 const mysql = require('mysql');
 
 const mySqlCon =mysql.createConnection({
-    host:'localhost',
-    user: 'root',
-    password:'password',
-    database:'examen',
+    host:'us-cdbr-east-02.cleardb.com',
+    user: 'b6bc5ac77907e4',
+    password:'952ff99c',
+    database:'heroku_e8750bf3785355a',
 
 });
+
+/**
+b6bc5ac77907e4 : 952ff99c @ us-cdbr-east-02.cleardb.com /heroku_e8750bf3785355a
+ */
 
 mySqlCon.connect( function (err){
 
